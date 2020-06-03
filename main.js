554db3f304e5b30291275ef4914964fcc10feb13
erinwangyy <59551284+erinwangyy@users.noreply.github.com>
2020-06-03 01:25:55 -0400

    fourth2 commitlots

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-$(function() {
+// $(function() {
 
-    $("#tv2").mousewheel(function(event, delta) {
+//     $("#tv2").mousewheel(function(event, delta) {
  
-       this.scrollLeft -= (delta * 30);
+//        this.scrollLeft -= (delta * 30);
      
-       event.preventDefault();
+//        event.preventDefault();
  
-    });
+//     });
  
- });
+//  });
+ function show(){
+
+   document.querySelector("#tv3").style.display="flex";
+ }
+
+//  function hide(){
+//     document.querySelector(".top").style.opacity=0.3;
+//  }
