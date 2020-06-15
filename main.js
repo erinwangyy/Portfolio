a5b3d65d2c8218f2746f8e26cf76b5baccde8736
erinwangyy <59551284+erinwangyy@users.noreply.github.com>
2020-06-14 22:53:34 -0400

    fourth2 commitlscrolls

--- main.js
+++ main.js
@@ -10,10 +10,15 @@
  
 //  });
  function show(){
-
+ $(".preview").animate({scrollTop:3000}, 800, 'swing', );
    document.querySelector("#tv3").style.display="flex";
  }
 
 //  function hide(){
 //     document.querySelector(".top").style.opacity=0.3;
 //  }
+
+// var scrollInterval = setInterval(show(){ 
+//    document.querySelector(".preview").scrollTop = document.documentElement.scrollHeight;
+// }, 50);
+
